e32957763982bafd84fcb53ecffa5ae3d2b3843c
Mickey374 <asantemichael37483@gmail.com>
2021-09-18 16:03:02 +0000

    A try and except block to handle running the program on a CLI if Driver is missing from PATH

--- run.py
+++ run.py
@@ -1,11 +1,19 @@
 from booking.booking import Booking
 
-with Booking() as bot:
-    bot.land_first_page()
-    bot.currency_convert(currency='GHS')
-    bot.place_to_go('Pisa')
-    bot.select_dates(check_in_date='2021-09-16',
-                     check_out_date='2021-09-30')
-    bot.choose_adults(10)
-    bot.click_search()
-    bot.apply_filterations()
+try:
+    with Booking() as bot:
+        bot.land_first_page()
+        bot.currency_convert(currency='GHS')
+        bot.place_to_go('Pisa')
+        bot.select_dates(check_in_date='2021-09-16', check_out_date='2021-09-30')
+        bot.choose_adults(10)
+        bot.click_search()
+        bot.apply_filterations()
+
+except Exception as e:
+    if 'in PATH' in str(e):
+        print("There is a problem with the Driver PATH")
+    
+    else:
+        raise
+
